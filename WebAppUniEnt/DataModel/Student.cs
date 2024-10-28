f803845146deedc1ee2564f9596f142115e7e0a4
Sebastiano Cantore <58028990+SCantore95@users.noreply.github.com>
2024-10-28 12:46:07 +0100

    Add files via upload

---
+++ WebAppUniEnt/DataModel/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAppUniEnt.DataModel;
+
+public partial class Student
+{
+    public string Matricola { get; set; } = null!;
+
+    public string Department { get; set; } = null!;
+
+    public DateTime? AnnoDiIscrizione { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string SureName { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public string Gender { get; set; } = null!;
+}
